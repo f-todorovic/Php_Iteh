3a4c6de259ca29d1024977533da7219ae3b55da3
Filip Todorovic <ftodorovic@hotmail.com>
2021-11-22 17:50:32 +0100

    Promene odredjenih konstanti za rad sa bazom

--- connect_vars.php
+++ connect_vars.php
@@ -1,8 +1,8 @@
 <?php
 
-    define("DB_HOST","localhost");
-    define("DB_USER","admin");
-    define("DB_PASSWORD","admin");
-    define("DB_NAME","database-iteh");
+define("DB_HOST","127.0.0.1:3307");
+define("DB_USER","admin");
+define("DB_PASSWORD","admin");
+define("DB_NAME","database-iteh");
 
 ?>
